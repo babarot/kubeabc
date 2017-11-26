28bb715369d31ae7d270a23cb57f3fbb7e9e2dcf
b4b4r07 <b4b4r07@gmail.com>
2017-11-26 20:50:34 +0900

    Update alias.zsh

--- scripts/alias.zsh
+++ scripts/alias.zsh
@@ -6,17 +6,11 @@ alias fzfkubernetesalias="fzf --height 25 --header-lines=1 --reverse --multi --c
 alias -g P='$(kubectl get pods | fzfkubernetesalias | awk "{print \$1}")'
 
 # Like P, global aliases about kubernetes resources
-alias -g PO='$(kubectl get pods | fzfkubernetesalias | awk "{print \$1}")'
-alias -g NS='$(kubectl get ns   | fzfkubernetesalias | awk "{print \$1}")'
-
-# alias -g DEPLOY='$(kubectl get deploy| fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
-# alias -g     RS='$(kubectl get rs    | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
-# alias -g    SVC='$(kubectl get svc   | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
-# alias -g    ING='$(kubectl get ing   | fzf-tmux --header-lines=1 --reverse --multi --cycle | awk "{print \$1}")'
-
-# Context switcher
-# c.f. https://github.com/ahmetb/kubectx
-alias kubectl-change='kubectx $(kubectx | fzy) >/dev/null'
+alias -g  PO='$(kubectl get pods | fzfkubernetesalias | awk "{print \$1}")'
+alias -g  NS='$(kubectl get ns   | fzfkubernetesalias | awk "{print \$1}")'
+alias -g  RS='$(kubectl get rs   | fzfkubernetesalias | awk "{print \$1}")'
+alias -g SVC='$(kubectl get svc  | fzfkubernetesalias | awk "{print \$1}")'
+alias -g ING='$(kubectl get ing  | fzfkubernetesalias | awk "{print \$1}")'
 
 # References
 # - https://github.com/c-bata/kube-prompt
